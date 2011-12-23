3a16129cc4c02a01817c8509e9a52496b5f4fefc
Rainer Kuhn <rkuhn@littleweblab.com>
2011-12-23 15:01:05 +0100

    215

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,17 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 #   code ...
 # end
 
+class DiceSet
+  attr_accessor :values
+  def initialize
+    self.values = []
+  end
+  def roll(rolls)
+    self.values = []
+    rolls.times{self.values << rand(6) + 1}
+  end
+end
+
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
